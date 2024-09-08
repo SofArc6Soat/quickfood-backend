07a8657c34ca838448e2f37b8d180c56a933e8fa
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-09-08 12:03:40 -0300

    feat: produto testes

---
+++ tests/Domain.Tests/TestHelpers/ProdutoFakeDataFactory.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+using Infra.Dto;
+
+namespace Domain.Tests.TestHelpers;
+
+public static class ProdutoFakeDataFactory
+{
+    public static Produto CriarProdutoValido()
+    {
+        return new Produto(Guid.NewGuid(), "Produto Exemplo", "Descrição do Produto", 100.00m, Categoria.Lanche, true);
+    }
+
+    public static ProdutoDb CriarProdutoDbValido()
+    {
+        return new ProdutoDb
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Produto Exemplo",
+            Descricao = "Descrição do Produto",
+            Preco = 100.00m,
+            Categoria = Categoria.Lanche.ToString(),
+            Ativo = true
+        };
+    }
+}
