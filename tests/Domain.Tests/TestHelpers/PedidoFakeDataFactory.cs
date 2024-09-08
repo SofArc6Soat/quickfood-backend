46a25eb847622bd433bec603578d24bd94f6431c
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-09-08 13:03:57 -0300

    feat: pedido teste

---
+++ tests/Domain.Tests/TestHelpers/PedidoFakeDataFactory.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+
+namespace Domain.Tests.TestHelpers;
+
+public static class PedidoFakeDataFactory
+{
+    public static Pedido CriarPedidoValido()
+    {
+        return new Pedido(Guid.NewGuid(), 1, Guid.NewGuid(), PedidoStatus.Rascunho, 100.00m, DateTime.Now);
+    }
+
+    public static Pedido CriarPedidoInvalido()
+    {
+        return new Pedido(Guid.NewGuid(), 0, null, PedidoStatus.Rascunho, -100.00m, DateTime.MinValue);
+    }
+
+    public static PedidoItem CriarPedidoItemValido()
+    {
+        return new PedidoItem(Guid.NewGuid(), 2, 10.00m);
+    }
+}
